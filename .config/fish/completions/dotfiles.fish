60100f46e0fdb2e9ad9c6944961cd471646ced3e
Mikhail Sharashin <mikhail.sharashin@bki-okb.ru>
2024-10-04 19:29:05 +0500

    modified dotfiles fish autocomplete

--- .config/fish/completions/dotfiles.fish
+++ .config/fish/completions/dotfiles.fish
@@ -1,4 +1,3 @@
 complete -c dotfiles -n "__fish_use_subcommand" -a "add" -d "Add file contents to the index"
-# complete -c dotfiles -n "__fish_seen_subcommand_from add" -a "(command ls -1A)" -d "Files and directories in the current directory (including hidden)"
 complete -c dotfiles -n "__fish_seen_subcommand_from add" -a "(__fish_complete_suffix)" -d "Files and directories in the current directory (including hidden)"
 
--- .config/fish/functions/dotfiles.fish
+++ .config/fish/functions/dotfiles.fish
@@ -1,4 +1,3 @@
 function dotfiles --wraps='git' --description 'alias config git --git-dir=/Users/msharashin/.my_config/ --work-tree=/Users/msharashin'
-    DOTFILES=1 git --git-dir=/Users/msharashin/.dotfiles/ --work-tree=/Users/msharashin $argv
-
+    git --git-dir=/Users/msharashin/.dotfiles/ --work-tree=/Users/msharashin $argv
 end
