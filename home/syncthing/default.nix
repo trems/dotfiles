23e95c5a83c4ea596df3e56c40d2f98b0fb274b7
Mikhail Sharashin <mikhail.sharashin@bki-okb.ru>
2025-03-30 23:11:14 +0500

    add obsidian folder to syncthing

--- home/syncthing/default.nix
+++ home/syncthing/default.nix
@@ -9,11 +9,21 @@
 {
   services.syncthing = {
     enable = true;
-    tray = true;
+    tray = false; # not supported on macos
+    guiAddress = "127.0.0.1:8384";
     settings = {
       devices = {
         phone = {
           id = "KYC7B2F-WL7SSA3-MFC6YQL-ZGMZWLF-XQB6LZR-HVUN3DI-QK5F5AB-TQDBPQL";
+        };
+      };
+      folders = {
+        "obsidian-vaults" = {
+          enable = true;
+          id = "9uxkv-kkef5";
+          path = "~/obsidian";
+          devices = [ "phone" ];
+          type = "sendreceive";
         };
       };
     };
