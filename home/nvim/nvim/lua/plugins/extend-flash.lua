6f3190a68fbbefe7997872b0c4438f4a9a71a79c
trems <trems95@gmail.com>
2025-04-15 14:33:54 +0500

    changes from IT-MAC-NB165.local on Tue Apr 15 14:33:53 +05 2025

--- home/nvim/nvim/lua/plugins/extend-flash.lua
+++ home/nvim/nvim/lua/plugins/extend-flash.lua
@@ -6,5 +6,34 @@ return {
   --       return "йцукенг"
   --     end,
   --   },
-  -- },
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  config = function()
+    local flash = require("flash")
+    local langmapper = require("langmapper")
+
+    for _, mode in pairs({ "n", "x", "o" }) do
+      langmapper.original_set_keymap(mode, "s", "", {
+        nowait = true,
+        desc = "Flash",
+        callback = function()
+          flash.jump()
+        end,
+      })
+      langmapper.original_set_keymap(mode, "ы", "", {
+        nowait = true,
+        desc = "Flash",
+        callback = function()
+          flash.jump({
+            labels = "олджавыфгнрткепимйцуячсшщзьбюАВЫФОЛДЖЙЦУКЕНГШЩЗ",
+          })
+        end,
+      })
+    end
+
+    ---@type Flash.Config
+    local opts = { modes = { char = { enabled = false } } }
+
+    flash.setup(opts)
+  end, -- },
 }
