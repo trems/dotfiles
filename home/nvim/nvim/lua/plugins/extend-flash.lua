049df9544a80238d8dcde153183950bf34a24441
trems <trems95@gmail.com>
2025-04-15 14:35:28 +0500

    changes from IT-MAC-NB165.local on Tue Apr 15 14:35:27 +05 2025

--- home/nvim/nvim/lua/plugins/extend-flash.lua
+++ home/nvim/nvim/lua/plugins/extend-flash.lua
@@ -1,31 +1,32 @@
-return {
-  "folke/flash.nvim",
-  config = function()
-    local flash = require("flash")
-    local langmapper = require("langmapper")
+return {}
+  or {
+    "folke/flash.nvim",
+    config = function()
+      local flash = require("flash")
+      local langmapper = require("langmapper")
 
-    for _, mode in pairs({ "n", "x", "o" }) do
-      langmapper.original_set_keymap(mode, "s", "", {
-        nowait = true,
-        desc = "Flash",
-        callback = function()
-          flash.jump()
-        end,
-      })
-      langmapper.original_set_keymap(mode, "ы", "", {
-        nowait = true,
-        desc = "Flash",
-        callback = function()
-          flash.jump({
-            labels = "олджавыфгнрткепимйцуячсшщзьбюАВЫФОЛДЖЙЦУКЕНГШЩЗ",
-          })
-        end,
-      })
-    end
+      for _, mode in pairs({ "n", "x", "o" }) do
+        langmapper.original_set_keymap(mode, "s", "", {
+          nowait = true,
+          desc = "Flash",
+          callback = function()
+            flash.jump()
+          end,
+        })
+        langmapper.original_set_keymap(mode, "ы", "", {
+          nowait = true,
+          desc = "Flash",
+          callback = function()
+            flash.jump({
+              labels = "олджавыфгнрткепимйцуячсшщзьбюАВЫФОЛДЖЙЦУКЕНГШЩЗ",
+            })
+          end,
+        })
+      end
 
-    ---@type Flash.Config
-    local opts = { modes = { char = { enabled = false } } }
+      ---@type Flash.Config
+      local opts = { modes = { char = { enabled = false } } }
 
-    flash.setup(opts)
-  end,
-}
+      flash.setup(opts)
+    end,
+  }
