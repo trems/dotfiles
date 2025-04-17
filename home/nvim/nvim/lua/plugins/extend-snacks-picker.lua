d02c4425bd6d8cc88d395dabcbd3d51428c43606
trems <trems95@gmail.com>
2025-04-17 12:53:01 +0500

    changes from IT-MAC-NB165.local on Thu Apr 17 12:53:00 +05 2025

--- home/nvim/nvim/lua/plugins/extend-snacks-picker.lua
+++ home/nvim/nvim/lua/plugins/extend-snacks-picker.lua
@@ -1,3 +1,39 @@
+local copy_file_path = function(_, item)
+  if not item then
+    return
+  end
+
+  local vals = {
+    ["BASENAME"] = vim.fn.fnamemodify(item.file, ":t:r"),
+    ["EXTENSION"] = vim.fn.fnamemodify(item.file, ":t:e"),
+    ["FILENAME"] = vim.fn.fnamemodify(item.file, ":t"),
+    ["PATH"] = item.file,
+    ["PATH (CWD)"] = vim.fn.fnamemodify(item.file, ":."),
+    ["PATH (HOME)"] = vim.fn.fnamemodify(item.file, ":~"),
+    ["URI"] = vim.uri_from_fname(item.file),
+  }
+
+  local options = vim.tbl_filter(function(val)
+    return vals[val] ~= ""
+  end, vim.tbl_keys(vals))
+  if vim.tbl_isempty(options) then
+    vim.notify("No values to copy", vim.log.levels.WARN)
+    return
+  end
+  table.sort(options)
+  vim.ui.select(options, {
+    prompt = "Choose to copy to clipboard:",
+    format_item = function(list_item)
+      return ("%s: %s"):format(list_item, vals[list_item])
+    end,
+  }, function(choice)
+    local result = vals[choice]
+    if result then
+      vim.fn.setreg("+", result)
+      Snacks.notify.info("Yanked `" .. result .. "`")
+    end
+  end)
+end
 return {
   {
     "folke/snacks.nvim",
