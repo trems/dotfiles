f860462f7e0962ce4c9d04367513c8264aafd2c5
trems <trems95@gmail.com>
2025-04-17 12:58:51 +0500

    changes from IT-MAC-NB165.local on Thu Apr 17 12:58:50 +05 2025

--- home/nvim/nvim/lua/plugins/extend-snacks-picker.lua
+++ home/nvim/nvim/lua/plugins/extend-snacks-picker.lua
@@ -71,6 +71,19 @@ local explorer_find_in_directory = function(_, item)
   })
 end
 
+local explorer_diff = function(picker)
+  picker:close()
+  local sel = picker:selected()
+  if #sel > 0 and sel then
+    Snacks.notify.info(sel[1].file)
+    vim.cmd("tabnew " .. sel[1].file)
+    vim.cmd("vert diffs " .. sel[2].file)
+    Snacks.notify.info("Diffing " .. sel[1].file .. " against " .. sel[2].file)
+    return
+  end
+
+  Snacks.notify.info("Select two entries for the diff")
+end
 return {
   {
     "folke/snacks.nvim",
