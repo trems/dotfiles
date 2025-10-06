49238bdee5c5276f9ebf7ffd5f4bb6038de6a6f9
trems <trems95@gmail.com>
2025-10-06 11:02:47 +0500

    changes from IT-MAC-NB165.local on Mon Oct 6 11:02:46 +05 2025

--- home/wezterm/wezterm/appearance.lua
+++ home/wezterm/wezterm/appearance.lua
@@ -60,11 +60,11 @@ function module.apply_to_config(config)
 		title = wez.truncate_right(title, max_width - 2)
 		local edge_foreground = background
 		return {
-			-- { Background = { Color = edge_background } },
-			-- { Foreground = { Color = edge_foreground } },
+			{ Background = { Color = edge_background } },
+			{ Foreground = { Color = edge_foreground } },
 			{ Text = wez.nerdfonts.pl_right_hard_divider },
-			-- { Background = { Color = background } },
-			-- { Foreground = { Color = foreground } },
+			{ Background = { Color = background } },
+			{ Foreground = { Color = foreground } },
 			{ Text = title },
 			{ Background = { Color = edge_background } },
 			{ Foreground = { Color = edge_foreground } },
