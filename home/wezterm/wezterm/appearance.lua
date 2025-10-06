1b5bfc53c03410516cfba7fe96c5e0d3985f5824
trems <trems95@gmail.com>
2025-10-06 10:43:13 +0500

    changes from IT-MAC-NB165.local on Mon Oct 6 10:43:12 +05 2025

--- home/wezterm/wezterm/appearance.lua
+++ home/wezterm/wezterm/appearance.lua
@@ -16,7 +16,7 @@ function module.apply_to_config(config)
 
 	config.colors = wez.color.get_builtin_schemes()[config.color_scheme]
 	local bg = wez.color.parse(config.colors.background)
-	local fg = wez.color.parse(config.colors.background)
+	local fg = wez.color.parse(config.colors.foreground)
 	local bg_dark = bg:darken(0.3)
 
 	config.window_frame = {
@@ -64,7 +64,7 @@ function module.apply_to_config(config)
 
 		if tab.is_active then
 			background = bg
-			foreground = "white"
+			foreground = fg
 		elseif hover then
 			background = bg:lighten(0.1)
 			foreground = "grey"
