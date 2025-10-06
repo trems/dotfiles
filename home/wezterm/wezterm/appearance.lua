25fa8827d400ff514f56a958da332f25f12a2da9
trems <trems95@gmail.com>
2025-10-06 10:24:54 +0500

    changes from IT-MAC-NB165.local on Mon Oct 6 10:24:53 +05 2025

--- home/wezterm/wezterm/appearance.lua
+++ home/wezterm/wezterm/appearance.lua
@@ -58,6 +58,15 @@ function module.apply_to_config(config)
 
 		if tab.is_active then
 			return {
+				{ Background = { Color = edge_background } },
+				{ Foreground = { Color = edge_foreground } },
+				{ Text = SOLID_LEFT_ARROW },
+				{ Background = { Color = background } },
+				{ Foreground = { Color = foreground } },
+				{ Text = title },
+				{ Background = { Color = edge_background } },
+				{ Foreground = { Color = edge_foreground } },
+				{ Text = SOLID_RIGHT_ARROW },
 				{ Text = SOLID_LEFT_ARROW },
 				{ Text = title },
 			}
