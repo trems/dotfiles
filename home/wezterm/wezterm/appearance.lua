986c356feb81e984122bdd24d9a0777a1e0ce150
trems <trems95@gmail.com>
2025-10-06 11:02:36 +0500

    changes from IT-MAC-NB165.local on Mon Oct 6 11:02:35 +05 2025

--- home/wezterm/wezterm/appearance.lua
+++ home/wezterm/wezterm/appearance.lua
@@ -27,9 +27,9 @@ function module.apply_to_config(config)
 	config.use_fancy_tab_bar = false
 	config.tab_max_width = 25
 
-  config.tab_bar_style = {
-    new_tab
-  }
+	config.tab_bar_style = {
+		new_tab,
+	}
 
 	wez.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
 		local pane = tab.active_pane
@@ -63,8 +63,8 @@ function module.apply_to_config(config)
 			-- { Background = { Color = edge_background } },
 			-- { Foreground = { Color = edge_foreground } },
 			{ Text = wez.nerdfonts.pl_right_hard_divider },
-			{ Background = { Color = background } },
-			{ Foreground = { Color = foreground } },
+			-- { Background = { Color = background } },
+			-- { Foreground = { Color = foreground } },
 			{ Text = title },
 			{ Background = { Color = edge_background } },
 			{ Foreground = { Color = edge_foreground } },
