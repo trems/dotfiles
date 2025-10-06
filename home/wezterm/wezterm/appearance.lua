7aeb035d1f7020762d4132eb5fd78af5d9855cee
trems <trems95@gmail.com>
2025-10-06 10:36:38 +0500

    changes from IT-MAC-NB165.local on Mon Oct 6 10:36:37 +05 2025

--- home/wezterm/wezterm/appearance.lua
+++ home/wezterm/wezterm/appearance.lua
@@ -65,7 +65,7 @@ function module.apply_to_config(config)
 			background = bg --"#2b2042"
 			foreground = "white" --"#c0c0c0"
 		elseif hover then
-			background = "#3b3052"
+			background = bg::lighten(0.3)
 			foreground = "#909090"
 		end
 
