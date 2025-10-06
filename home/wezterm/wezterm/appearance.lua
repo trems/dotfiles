e7a3214b80db860da6bd40d0cdf5923b4786c5a4
trems <trems95@gmail.com>
2025-10-06 10:58:01 +0500

    changes from IT-MAC-NB165.local on Mon Oct 6 10:58:00 +05 2025

--- home/wezterm/wezterm/appearance.lua
+++ home/wezterm/wezterm/appearance.lua
@@ -50,7 +50,7 @@ function module.apply_to_config(config)
 			foreground = "#f1f1f1"
 		elseif hover then
 			background = bg:darken(0.1)
-			foreground = #cfcfcf
+			foreground = "#cfcfcf""
 		end
 
 		title = wez.truncate_right(title, max_width - 2)
