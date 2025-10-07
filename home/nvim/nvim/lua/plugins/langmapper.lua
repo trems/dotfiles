46583b310ac59c703e6bdc93b21dd7ab1df2e0cf
trems <trems95@gmail.com>
2025-10-07 12:34:41 +0500

    changes from IT-MAC-NB165.local on Tue Oct 7 12:34:39 +05 2025

--- home/nvim/nvim/lua/plugins/langmapper.lua
+++ home/nvim/nvim/lua/plugins/langmapper.lua
@@ -10,21 +10,6 @@ return {
     "folke/which-key.nvim",
     opts = function(_, opts)
       local translate_key = require("langmapper.utils").translate_keycode
-      -- local wk_state = require("which-key.state")
-      -- local check_orig = wk_state.check
-
-      -- wk_state.check = function(state, key) ---@diagnostic disable-line: duplicate-set-field
-      --   if key ~= nil then
-      --     key = translate_key(key, "default", "ru")
-      --   end
-      --   if state.node.key ~= nil then
-      --     state.node.key = translate_key(state.node.key, "default", "ru")
-      --   end
-      --
-      --   return check_orig(state, key)
-      -- end
-
-      -- don't show mappings translated by langmapper.nvim. Show entry if func returns true
       opts.filter = function(mapping)
         return mapping.lhs
           and mapping.lhs == translate_key(mapping.lhs, "default", "ru")
