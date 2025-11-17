e291da56133a1b1fed9addce4ef11ec5fe6022c8
trems <trems95@gmail.com>
2025-11-17 12:48:19 +0500

    changes from IT-MAC-NB165.local on Mon Nov 17 12:48:17 +05 2025

--- flake.nix
+++ flake.nix
@@ -15,68 +15,64 @@
     colmena.url = "github:zhaofengli/colmena";
   };
 
-  outputs =
-    inputs@{
-      self,
-      darwin,
-      nixpkgs,
-      home-manager,
-      colmena,
-    }:
-    let
-      systemDarwin = "aarch64-darwin";
-      pkgsDarwin = import nixpkgs { system = systemDarwin; };
-      nixSettings = user: {
-        settings = {
-          trusted-users = [ user ];
-          extra-experimental-features = [
-            "nix-command"
-            "flakes"
-          ];
-        };
-      };
-      darwinSystem =
-        {
-          user,
-          arch ? systemDarwin,
-        }:
-        darwin.lib.darwinSystem {
-          system = arch;
-          modules = [
-            ./darwin/darwin.nix
-            home-manager.darwinModules.home-manager
-            {
-              _module.args = { inherit inputs; };
-              home-manager = {
-                users.${user} = import ./home;
-                sharedModules = [ ];
-              };
-              users.users.${user}.home = "/Users/${user}";
-              nix = nixSettings user;
-            }
-          ];
-        };
-    in
-    {
-      # Build darwin flake using:
-      # $ sudo darwin-rebuild build --flake .#IT-MAC-NB165
-      darwinConfigurations = {
-        "IT-MAC-NB165" = darwinSystem {
-          user = "msharashin";
-        };
-      };
-
-      # nix develop
-      devShells."${systemDarwin}".default = pkgsDarwin.mkShell {
-        buildInputs = with pkgsDarwin; [
-          colmena.defaultPackage.${systemDarwin}
-          fish
-          sops
-          age
+  outputs = inputs @ {
+    self,
+    darwin,
+    nixpkgs,
+    home-manager,
+    colmena,
+  }: let
+    systemDarwin = "aarch64-darwin";
+    pkgsDarwin = import nixpkgs {system = systemDarwin;};
+    nixSettings = user: {
+      settings = {
+        trusted-users = [user];
+        extra-experimental-features = [
+          "nix-command"
+          "flakes"
         ];
-        shellHook = ''
-          exec fish
-        '';
       };
     };
+    darwinSystem = {
+      user,
+      arch ? systemDarwin,
+    }:
+      darwin.lib.darwinSystem {
+        system = arch;
+        modules = [
+          ./darwin/darwin.nix
+          home-manager.darwinModules.home-manager
+          {
+            _module.args = {inherit inputs;};
+            home-manager = {
+              users.${user} = import ./home;
+              sharedModules = [];
+            };
+            users.users.${user}.home = "/Users/${user}";
+            nix = nixSettings user;
+          }
+        ];
+      };
+  in {
+    # Build darwin flake using:
+    # $ sudo darwin-rebuild build --flake .#IT-MAC-NB165
+    darwinConfigurations = {
+      "IT-MAC-NB165" = darwinSystem {
+        user = "msharashin";
+      };
+    };
+
+    # nix develop
+    devShells."${systemDarwin}".default = pkgsDarwin.mkShell {
+      buildInputs = with pkgsDarwin; [
+        colmena.defaultPackage.${systemDarwin}
+        fish
+        sops
+        age
+      ];
+      shellHook = ''
+        exec fish
+      '';
+    };
+  };
 }
