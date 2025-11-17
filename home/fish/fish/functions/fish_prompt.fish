f3d31af36492111c05926e6b3e60568adec14af6
trems <trems95@gmail.com>
2025-11-17 14:03:42 +0500

    changes from IT-MAC-NB165.local on Mon Nov 17 14:03:41 +05 2025

--- home/fish/fish/functions/fish_prompt.fish
+++ home/fish/fish/functions/fish_prompt.fish
@@ -53,7 +53,7 @@ function fish_prompt
 
     set -l nix_shell_info (
       if test -n "$IN_NIX_SHELL"
-          set delim $delim' '
+          # set -e delim $delim' '
           echo -n "<nix-shell> "
       end
     )
