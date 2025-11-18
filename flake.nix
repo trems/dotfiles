638a6bd86fdb412629c6b38f550970b3e63acff9
trems <trems95@gmail.com>
2025-11-18 17:11:53 +0500

    changes from IT-MAC-NB165.local on Tue Nov 18 17:11:52 +05 2025

--- flake.nix
+++ flake.nix
@@ -82,7 +82,7 @@
             hostname = "192.168.0.102";
             profiles.system = {
               user = "mike";
-              path = deploy-rs.lib.${systemLinux}.nix
+              path = deploy-rs.lib.${systemLinux}.activate.nixos self.
             };
           };
         };
