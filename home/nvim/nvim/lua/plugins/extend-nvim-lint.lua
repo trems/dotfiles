2b6a64c368827543d8b8803406ae6bd91a9effbf
trems <trems95@gmail.com>
2025-12-04 12:09:12 +0500

    changes from IT-MAC-NB165.local on Thu Dec 4 12:09:11 +05 2025

--- home/nvim/nvim/lua/plugins/extend-nvim-lint.lua
+++ home/nvim/nvim/lua/plugins/extend-nvim-lint.lua
@@ -12,21 +12,21 @@ return {
           args = { "lint", "--format=json", "-" },
           stdin = true,
         },
-        -- golangcilint = {
-        --   condition = function(ctx)
-        --     -- exclude files from 'vendor' folder
-        --     local root_dir = vim.fs.root(ctx.filename, { "go.mod", "go.work" })
-        --
-        --     for parent_dir in vim.fs.parents(ctx.filename) do
-        --       local dir_name = vim.fs.basename(parent_dir)
-        --       if dir_name == "vendor" and vim.fs.dirname(parent_dir) == root_dir then
-        --         return false
-        --       end
-        --     end
-        --
-        --     return true
-        --   end,
-        -- },
+        golangcilint = {
+          condition = function(ctx)
+            -- exclude files from 'vendor' folder
+            local root_dir = vim.fs.root(ctx.filename, { "go.mod", "go.work" })
+
+            for parent_dir in vim.fs.parents(ctx.filename) do
+              local dir_name = vim.fs.basename(parent_dir)
+              if dir_name == "vendor" and vim.fs.dirname(parent_dir) == root_dir then
+                return false
+              end
+            end
+
+            return true
+          end,
+        },
       },
     },
   },
