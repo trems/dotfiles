dccef099da257b845f5a16952a2e9a563a7573f4
trems <trems95@gmail.com>
2025-12-06 22:55:16 +0500

    changes from IT-MAC-NB165.local on Sat Dec 6 22:55:15 +05 2025

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,6 @@
 let
   msharashin = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKLeUpRLdLM9bNaZ2utFfHtw4MPIlj3vo6UjW2aFE9eA msharashin@IT-MAC-NB165.local";
-  home-laptop
+  home-laptop2 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIteJxePAOsUTL2ZANy1jXwzhbt/UepwU1U+Iq/1pkj1";
 in {
   "test1.age".publicKeys = [msharashin];
 }
