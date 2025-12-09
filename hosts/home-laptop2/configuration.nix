8a6d6035d11f1f40934cf0aab0e0bb25d8d9d063
trems <trems95@gmail.com>
2025-12-09 15:51:29 +0500

    changes from IT-MAC-NB165.local on Tue Dec 9 15:51:28 +05 2025

--- hosts/home-laptop2/configuration.nix
+++ hosts/home-laptop2/configuration.nix
@@ -75,7 +75,7 @@ in {
             ];
           };
           allowlists = {
-            default = ["https://raw.githubusercontent.com/anudeepND/whitelist/refs/heads/master/domains/whitelist.txt"];
+            # default = ["https://raw.githubusercontent.com/anudeepND/whitelist/refs/heads/master/domains/whitelist.txt"];
           };
           #Configure what block categories are used
           clientGroupsBlock = {
