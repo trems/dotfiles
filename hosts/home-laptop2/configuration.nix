e9a0d55166e2c5e8f49782b223994d7242830034
trems <trems95@gmail.com>
2025-12-09 15:51:39 +0500

    changes from IT-MAC-NB165.local on Tue Dec 9 15:51:38 +05 2025

--- hosts/home-laptop2/configuration.nix
+++ hosts/home-laptop2/configuration.nix
@@ -72,6 +72,7 @@ in {
               "https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling/hosts"
               "https://blocklistproject.github.io/Lists/ads.txt"
               "https://github.com/Zalexanninev15/NoADS_RU/raw/refs/heads/main/hosts/blockerFL.txt"
+              ""
             ];
           };
           allowlists = {
