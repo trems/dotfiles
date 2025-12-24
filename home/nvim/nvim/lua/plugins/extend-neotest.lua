4d185ee4488163e35e104831b29da7d96359083d
trems <trems95@gmail.com>
2025-12-24 17:38:34 +0500

    changes from IT-MAC-NB165.local on Wed Dec 24 17:38:32 +05 2025

--- home/nvim/nvim/lua/plugins/extend-neotest.lua
+++ home/nvim/nvim/lua/plugins/extend-neotest.lua
@@ -11,6 +11,7 @@ return {
           -- Here we can set options for neotest-golang, e.g.
           -- go_test_args = { "-v", "-race", "-count=1", "-timeout=60s" },
           testify_enabled = true,
+          filter_dirs = {}
         },
       },
     },
