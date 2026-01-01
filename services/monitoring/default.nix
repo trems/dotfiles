4ac601ded1e0d019b25c94a4cd8552b542754e6c
trems <trems95@gmail.com>
2026-01-01 20:49:35 +0500

    changes from IT-MAC-NB165.local on Thu Jan 1 20:49:34 +05 2026

--- services/monitoring/default.nix
+++ services/monitoring/default.nix
@@ -44,7 +44,6 @@ in {
           {
             name = "VictoriaMetrics";
             type = "victoriametrics-metrics-datasource";
-            # type = "prometheus";
             access = "proxy";
             url = "http://127.0.0.1:${toString vmPort}";
             isDefault = true;
