9eab9a5ca28aef46c13845dfcbe3a55fbc2fb01b
trems <trems95@gmail.com>
2026-03-24 23:42:24 +0500

    rekey with new pubkey for mbp machine

--- secrets/pubkeys.nix
+++ secrets/pubkeys.nix
@@ -1,7 +1,7 @@
 rec {
   home-laptop2 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIteJxePAOsUTL2ZANy1jXwzhbt/UepwU1U+Iq/1pkj1";
   macbook-air-m1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDeZ2pZ4j1wQReMRGYSe2JV/DVrVjlccBBiIOnpoW/gk m@macbook-air-m1.local";
-  mbp = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKQ2fKPpQzJBbTpT0Vtw78vdPGRAsavDNHc2NkdAEBwj m@mbp.local"
+  mbp = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKQ2fKPpQzJBbTpT0Vtw78vdPGRAsavDNHc2NkdAEBwj m@mbp.local";
 
   all = [home-laptop2 macbook-air-m1 mbp];
 }
